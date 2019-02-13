d05d2e68b810026dcde9175989ccf068c866382b
colinbernard <colinjbernard@hotmail.com>
2019-02-13 14:57:32 -0800

    Deleted old provider pages. Version -> 0.3

--- version.php
+++ version.php
@@ -3,8 +3,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 
-$plugin->version   = 2019021200; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2019021300; // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2016112900; // Requires this Moodle version.
 $plugin->component = 'local_lti'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'v0.2';
+$plugin->release = 'v0.3';
