ba44f83c0deb0d974dc326589341b12d8498f430
colinbernard <colinjbernard@hotmail.com>
2019-02-27 14:29:58 -0800

    Removed lang parameter

--- lang/en/local_lti.php
+++ lang/en/local_lti.php
@@ -12,8 +12,8 @@ $string['error_invalid_type'] = 'Invalid LTI type in launch URL. Try ?type=book 
 $string['error_missing_required_params'] = 'Missing required parameters!';
 $string['error_auth_failed'] = 'Authentication failed! Not a valid launch request.';
 $string['error_launch_request'] = 'Not an LTI launch request.';
-$string['error_rendering_book'] = 'An error occurred while rendering the LTI book. The exception is: \'{$a}\'.';
-$string['error_rendering_page'] = 'An error occurred while rendering the LTI page. The exception is: \'{$a}\'.';
+$string['error_rendering_book'] = 'An error occurred while rendering the LTI book. Ensure that the ID you provided is correct.';
+$string['error_rendering_page'] = 'An error occurred while rendering the LTI page. Ensure that the ID you provided is correct.';
 $string['error_book_id'] = 'An error occurred while attempting to retrieve the book id. Check that the id you are providing is correct.';
 $string['error_page_id'] = 'An error occurred while attempting to retrieve the page id. Check that the id you are providing is correct.';
 $string['error_missing_type'] = 'LTI type parameter is missing from the launch URL. Try ?type=book or ?type=page.';
