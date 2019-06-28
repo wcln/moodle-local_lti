7954085d87cfe302004b0594a140621a0a898bf7
Colin Bernard <colin@lingellearning.com>
2019-06-27 22:01:48 -0700

    Version -> 1.0

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 
-$plugin->version   = 2019062000; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2019062700; // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2016112900; // Requires this Moodle version.
 $plugin->component = 'local_lti'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = 'v0.9';
+$plugin->release   = 'v1.0';
