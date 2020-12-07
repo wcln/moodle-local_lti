dd3376ef767bd69637938dd082b55fbc865a83f9
Colin Perepelken <colin@perepelken.ca>
2020-12-07 14:28:41 -0800

    Build statistic boxes at top of overview tab

---
+++ vue/components/partials/StatisticBox.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="box has-background-primary">
+    <article class="media">
+      <div v-if="icon !== false && icon.length > 0" class="media-left">
+            <i :class="'has-text-white fas fa-3x ' + icon"></i>
+      </div>
+      <div class="media-content">
+        <p class="is-size-4 has-text-white">
+          <strong class="has-text-white">{{number}}</strong> {{text}}
+        </p>
+      </div>
+    </article>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "StatisticBox",
+  props: {
+    icon: {
+      type: String,
+      default: false
+    },
+    text: {
+      type: String
+    },
+    number: {
+      type: String
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
--- vue/components/views/dashboard/Overview.vue
+++ vue/components/views/dashboard/Overview.vue
@@ -1,13 +1,50 @@
 <template>
-  <h1>Overview</h1>
+  <div class="overview-tab">
+    <div class="columns is-centered">
+      <div class="column" v-for="statBox in statisticBoxes">
+        <StatisticBox class="statbox" :icon="statBox.icon" :number="statBox.number" :text="statBox.text">
+          <p class="is-size-4 has-text-white" v-html="statBox.text"></p>
+        </StatisticBox>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import StatisticBox from "../../partials/StatisticBox";
+
 export default {
-name: "Overview"
+  name: "Overview",
+  components: {StatisticBox},
+  data() {
+    return {
+      statisticBoxes: [
+        {
+          number: "1.25 million",
+          text: "requests",
+          icon: "fa-exchange-alt"
+        },
+        {
+          number: "768",
+          text: "resources requested",
+          icon: "fa-book"
+        },
+        {
+          number: "40",
+          text: "active consumer sites",
+          icon: "fa-sitemap"
+        },
+        {
+          number: "0",
+          text: "errors in last 24 hours",
+          icon: "fa-bug"
+        },
+      ]
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 
 </style>
