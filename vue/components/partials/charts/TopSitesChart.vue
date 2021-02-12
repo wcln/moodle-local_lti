7bcb64e9524d42be777e3573cc555d3a73b4b76a
Colin Perepelken <colin@perepelken.ca>
2021-02-12 10:00:29 -0800

    Hook TopSitesChart up to web service

--- vue/components/partials/charts/TopSitesChart.vue
+++ vue/components/partials/charts/TopSitesChart.vue
@@ -11,23 +11,15 @@
 
 <script>
 import PieChart from "./base/PieChart";
+import {ajax} from "../../../store";
 
 export default {
   name: 'TopSitesChart',
   components: {PieChart},
   data() {
     return {
-      loaded: true,
-      chartdata: {
-        labels: ['Southeast Kootenay', 'Rocky Mountain', 'Burnaby', 'Central Okanagan', 'Vernon', 'Other sites'],
-        datasets: [{
-          label: 'Top consumer sites',
-          data: [7392, 3389, 3091, 1066, 739, 5004],
-          backgroundColor: [
-            '#8EC63F', '#2d7dd2', '#E84855', '#eba600', '#2d3047'
-          ]
-        }]
-      },
+      loaded: false,
+      chartdata: {},
       options: {
         responsive: true,
         maintainAspectRatio: false,
@@ -39,7 +31,25 @@ export default {
     }
   },
   async mounted() {
-    // TODO make an API call and load the chart data
+    ajax('local_lti_get_top_consumers', {}).then(sites => {
+
+      ajax('local_lti_get_total_requests_count', []).then(count => {
+
+        // Add an entry for total request count so we can compare top sites to this
+        sites.push({name: 'Other sites', access_count: count});
+
+        this.chartdata.labels = sites.map(site => site.name);
+        this.chartdata.datasets = [{
+          label: 'Top consumer sites',
+          backgroundColor: [
+            '#8EC63F', '#2d7dd2', '#E84855', '#eba600', '#2d3047', '#ededed'
+          ],
+          data: sites.map(site => site.access_count)
+        }];
+
+        this.loaded = true;
+      });
+    });
   }
 }
 </script>
