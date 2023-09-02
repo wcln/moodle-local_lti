251549e90cf895e24a5897d10db119e55b0b0cca
Colin Perepelken <colin@sparelabs.com>
2023-09-02 11:15:28 -0700

    Fix deprecated syntax for PHP 8

--- classes/imsglobal/lti/oauth/signature_method.php
+++ classes/imsglobal/lti/oauth/signature_method.php
@@ -77,7 +77,7 @@ abstract class signature_method
         // Avoid a timing leak with a (hopefully) time insensitive compare
         $result = 0;
         for ($i = 0; $i < strlen($signature); $i++) {
-            $result |= ord($built{$i}) ^ ord($signature{$i});
+            $result |= ord($built[$i]) ^ ord($signature[$i]);
         }
 
         return $result == 0;
